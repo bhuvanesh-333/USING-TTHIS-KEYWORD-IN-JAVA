0e33da83f00c83f6017fae5687cacf0a81bb8a2c
Bhuvanesh <bhuvanprabha4@gmail.com>
2025-11-14 21:26:06 +0530

    Add day9 class with try-catch-finally example

---
+++ day9.java
@@ -0,0 +1,15 @@
+public class day9 {
+
+    public static void main(String[] args) {
+        try {
+            int a = 10;
+            System.out.println(a);
+        }
+        catch(Exception e) {
+            System.out.println(e);
+        }
+        finally {
+            System.out.println("i am the final boss");
+        }
+    }
+}
